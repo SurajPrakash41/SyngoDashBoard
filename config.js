23f3178abcac08180fea9af19c7c5b9678fd6944
Prakash <suraj.prakash@siemens-healthineers.com>
2024-05-10 13:14:38 +0530

    Eh boy

---
+++ config.js
@@ -0,0 +1,624 @@
+window.CONFIG = {
+  macros: [
+    {
+      category: 'Work',
+      name: 'condeco',
+      triggers: [
+        'c',
+        'con',
+        'condeco',
+      ],
+      key: 'KeyCON',
+      icon: 'condeco',
+      url: 'https://siemens-healthineers.condecosoftware.com/master.aspx',
+      normalisedURL: 'siemens-healthineers.condecosoftware.com',
+      commands: {
+        go: {
+          template: '{@}'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+    
+    {
+      category: 'Work',
+      name: 'tfs',
+      triggers: [
+        'tf',
+        'tfs',
+        'tfs',
+      ],
+      key: 'KeyTFS',
+      icon: 'tfs',
+      url: 'https://tfs.healthcare.siemens.com:8090/tfs/SY.US/SyngoDynamics/',
+      normalisedURL: 'tfs.healthcare.siemens.com',
+      commands: {
+        go: {
+          template: '{@}'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+    {
+      category: 'Work',
+      name: 'workday',
+      triggers: [
+        'w',
+        'wd',
+        'workday',
+      ],
+      key: 'KeyWD',
+      icon: 'workday',
+      url: 'https://wd3.myworkday.com/healthineers/d/home.htmld/',
+      normalisedURL: 'tfs.healthcare.wd3.myworkday.com/healthineers/d/home.htmld.com',
+      commands: {
+        go: {
+          template: '{@}'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+    {
+      category: 'Work',
+      name: 'vmware',
+      triggers: [
+        'v',
+        'vm',
+        'vmware',
+      ],
+      key: 'KeyVM',
+      icon: 'vmware',
+      url: 'https://hccloud-hc.shpl.in.siemens-healthineers.com/',
+      normalisedURL: 'hccloud-hc.shpl.in.siemens-healthineers.com',
+      commands: {
+        go: {
+          template: '{@}'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+    {
+      category: 'Work',
+      name: 'Intranet',
+      triggers: [
+        'i',
+        'intra',
+        'intranet',
+      ],
+      key: 'KeyI',
+      icon: 'intranet',
+      url: 'https://healthineers.sharepoint.com/',
+      normalisedURL: 'healthineers.sharepoint.com',
+      commands: {
+        go: {
+          template: '{@}',
+          description: 'go to home page'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+    {
+      category: 'Work',
+      name: 'learn',
+      triggers: [
+        'le',
+        'lea',
+        'learn',
+        'learn4u'
+      ],
+      key: 'KeyLEARN',
+      icon: 'learn',
+      url: 'https://learn4u.sabacloud.com/',
+      normalisedURL: 'learn4u.sabacloud.com',
+      commands: {
+        go: {
+          template: '{@}'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+
+    {
+      category: 'Work',
+      name: 'TimeSheet',
+      triggers: [
+        't',
+        'time',
+        'timesheet',
+      ],
+      key: 'KeyTIME',
+      icon: 'timesheet',
+      url: 'https://dctools.siemens-healthineers.com/TMS_HC/General/TMSLogin.aspx',
+      normalisedURL: 'dctools.siemens-healthineers.com/TMS_HC/General/TMSLogin.aspx',
+      commands: {
+        go: {
+          template: '{@}',
+          description: 'go to home page'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+
+    {
+      category: 'Work',
+      name: 'Sharp',
+      triggers: [
+        's',
+        'sharp',
+        'portal',
+      ],
+      key: 'KeySHARP',
+      icon: 'sharp',
+      url: 'https://healthcare.service-now.com/serviceport',
+      normalisedURL: 'healthcare.service-now.com/serviceport',
+      commands: {
+        go: {
+          template: '{@}',
+          description: 'go to home page'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+
+
+    {
+      category: 'Work',
+      name: 'peakon',
+      triggers: [
+        'p',
+        'peakon',
+        'survey',
+      ],
+      key: 'KeyPeakon',
+      icon: 'peakon',
+      url: 'https://survey.peakon.com/survey/answer/nWhMHQb7I2BVXsZGVscRk4Rh3tkJJ6c3',
+      normalisedURL: 'survey.peakon.com/survey/answer/nWhMHQb7I2BVXsZGVscRk4Rh3tkJJ6c3',
+      commands: {
+        go: {
+          template: '{@}',
+          description: 'go to home page'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+
+
+
+    {
+      category: 'Work',
+      name: 'hr4u',
+      triggers: [
+        'hr',
+        'hr4',
+        '4',
+        'hr4u'
+      ],
+      key: 'KeyI',
+      icon: 'hr4u',
+      url: 'https://hrsp.siemens.cloud/portal/hr_in/in/welcome',
+      normalisedURL: 'hrsp.siemens.cloud',
+      commands: {
+        go: {
+          template: '{@}'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#E1DAE6', '#BB6215']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+
+    {
+      category: 'Programming',
+      name: 'ChatGPT',
+      icon: 'chatGPT',
+      url: 'https://chat.openai.com/chat',
+      normalisedURL: 'openai.com',
+      triggers: [
+        'c',
+        'chat',
+        'gpt',
+        'cgpt',
+        'chatgpt'
+      ],
+      key: 'KeyC',
+      bgColor: {
+        type: 'solid',
+        color: '#70a597',
+      },
+      textColor: '#f7f7f7',
+      pinned: true
+    },
+    {
+      category: 'Entertainment',
+      name: 'YouTube',
+      triggers: [
+        'y',
+        'yt',
+        'youtube',
+      ],
+      key: 'KeyY',
+      icon: 'youtube',
+      url: 'https://youtube.com',
+      normalisedURL: 'youtube.com',
+      commands: {
+        go: {
+          template: 'https://youtu.be/{$}',
+          description: 'go to video'
+        },
+        search: {
+          template: '{@}/results?search_query={$}'
+        }
+      },
+      bgColor: {
+        type: 'solid',
+        color: '#f30002'
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+    {
+      category: 'Communication',
+      name: 'WhatsApp',
+      icon: 'whatsApp',
+      url: 'https://web.whatsapp.com',
+      normalisedURL: 'whatsapp.com',
+      triggers: [
+        'w',
+        'wa',
+        'wapp',
+        'wahtsapp',
+      ],
+      key: 'KeyW',
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        angle: 45,
+        colors: ['#26d347', '#58f879']
+      },
+      textColor: '#fff',
+      pinned: true
+    },
+
+    {
+      category: 'Social',
+      name: 'Twitter',
+      icon: 'twitter',
+      url: 'https://twitter.com',
+      normalisedURL: 'twitter.com',
+      triggers: [
+        'tt',
+        'twt',
+        'twit',
+        'twitt',
+        'twitter'
+      ],
+      commands: {
+        search: {
+          template: '{@}/search?q={$}'
+        },
+        go: {
+          template: '{@}/{$}'
+        }
+      },
+      bgColor: {
+        type: 'solid',
+        color: '#1c99e6'
+      },
+      textColor: '#e8e8e8',
+      pinned: true
+    },
+
+
+    {
+      category: 'Programming',
+      name: 'StackOverflow',
+      icon: 'stackoverflow',
+      url: 'https://stackoverflow.com',
+      normalisedURL: 'stackoverflow.com',
+      triggers: [
+        's',
+        'st',
+        'so',
+        'stack',
+        'stackoverflow'
+      ],
+      key: 'KeyS',
+      commands: {
+        search: {
+          template: '{@}/search?q={$}'
+        },
+        go: {
+          template: '{@}/questions/{$}',
+          description: 'go to question'
+        }
+      },
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#e87922', '#ffbe25'],
+        stops: [0, 100]
+      },
+      textColor: '#212121',
+      pinned: true
+    },
+
+
+
+    {
+      category: 'Music',
+      name: 'spotify',
+      icon: 'spotify',
+      url: 'https://open.spotify.com',
+      normalisedURL: 'spotify.com',
+      triggers: [
+        'music',
+        'sp',
+        'sfy',
+        'spo',
+        'spotify'
+      ],
+      commands: {
+        search: {
+          template: '{@}/search/{$}'
+        },
+        go: {
+          template: '{@}/track/{$}',
+          description: 'go to track'
+        }
+      },
+      bgColor: {
+        type: 'solid',
+        color: '#1ccc5b'
+      },
+      textColor: '#000',
+      pinned: true
+    },
+
+    {
+      category: 'Programming',
+      name: 'GitHub',
+      icon: 'github',
+      url: 'https://github.com',
+      normalisedURL: 'github.com',
+      triggers: [
+        'g',
+        'git',
+        'github'
+      ],
+      key: 'KeyG',
+      commands: {
+        go: {
+          template: '{@}/{$}',
+          description: 'go to user'
+        },
+        search: {
+          template: '{@}/search?q={$}'
+        }
+      },
+      bgColor: {
+        type: 'solid',
+        color: '#171515'
+      },
+      textColor: '#e8e8e8',
+      pinned: true
+    },
+
+
+    {
+      category: 'Work',
+      name: 'Linkedin',
+      icon: 'linkedin',
+      key: 'KeyL',
+      triggers: [
+        'l',
+        'lk',
+        'li',
+        'linked',
+        'linkedin'
+      ],
+      url: 'https://linkedin.com',
+      normalisedURL: 'linkedin.com',
+      commands: {
+        search: {
+          template: '{@}/search/results/all/?keywords={$}'
+        }
+      },
+      bgColor: {
+        type: 'solid',
+        color: '#0077b7'
+      },
+      textColor: '#fff',
+      pinned: true
+    }
+
+
+
+  ],
+  commands: [
+    {
+      type: 'search',
+      trigger: '?'
+    },
+    {
+      type: 'go',
+      trigger: '/'
+    }
+  ],
+  engines: {
+    google: {
+      name: 'Google',
+      bgColor: {
+        type: 'solid',
+        color: '#aaa'
+      },
+      textColor: '#fff',
+      types: {
+        // @ - origin query (what user typed); $ - final query (what is in the query field (selected))
+        query: {
+          template: 'https://www.google.com/search?q={$}'
+        },
+        calculator: {
+          template: 'https://www.google.com/search?q={@}'
+        },
+        currency: {
+          template: 'https://www.google.com/search?q={@}'
+        }
+      }
+    },
+    duckDuckGo: {
+      name: 'DuckDuckGo',
+      bgColor: {
+        type: 'solid',
+        color: '#e37151'
+      },
+      textColor: '#e8e8e8',
+      types: {
+        query: {
+          template: 'https://duckduckgo.com/?q={$}'
+        },
+        calculator: {
+          template: 'https://duckduckgo.com/?q={@}'
+        },
+        currency: {
+          template: 'https://duckduckgo.com/?q={@}'
+        }
+      }
+    },
+    yandex: {
+      name: 'Yandex',
+      bgColor: {
+        type: 'solid',
+        color: '#fc3f1d'
+      },
+      textColor: '#000',
+      types: {
+        query: {
+          template: 'https://ya.ru/search/?text={$}'
+        },
+        calculator: {
+          template: 'https://ya.ru/search/?text={@}'
+        },
+        currency: {
+          template: 'https://ya.ru/search/?text={@}'
+        }
+      }
+    },
+    qwant: {
+      name: 'Qwant',
+      bgColor: {
+        type: 'solid',
+        color: '#5992f7'
+      },
+      textColor: '#000',
+      types: {
+        query: {
+          template: 'https://www.qwant.com/?q={$}'
+        },
+        calculator: {
+          template: 'https://www.qwant.com/?q={@}'
+        },
+        currency: {
+          template: 'https://www.qwant.com/?q={@}'
+        }
+      }
+    },
+    bing: {
+      name: 'Bing',
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#3ab0f1', '#2351dd', '#40d1da']
+      },
+      textColor: '#e8e8e8',
+      types: {
+        query: {
+          template: 'https://www.bing.com/search?q={$}'
+        },
+        calculator: {
+          template: 'https://www.bing.com/search?q={@}'
+        },
+        currency: {
+          template: 'https://www.bing.com/search?q={@}'
+        }
+      }
+    },
+    brave: {
+      name: 'Brave',
+      bgColor: {
+        type: 'gradient',
+        gradientType: 'linear',
+        colors: ['#FB542B', '#343546']
+      },
+      textColor: '#e8e8e8',
+      types: {
+        query: {
+          template: 'https://search.brave.com/search?q={$}'
+        },
+        calculator: {
+          template: 'https://search.brave.com/search?q={@}'
+        },
+        currency: {
+          template: 'https://search.brave.com/search?q={@}'
+        }
+      }
+    }
+  }
+}
